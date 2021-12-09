503baea09ea49501ba8bd22103bbecdef1f7a3e9
Sky <41401030+HeartfulSky@users.noreply.github.com>
2021-12-09 03:37:37 -0800

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
                 <div className="flex">
                     <Link href="/">
                         <a className="flex font-medium items-center mb-4 md:mb-0">
-                            <span className="text-2xl font-bold duration-200">Robert S.</span>
+                            <span className="text-2xl font-bold duration-200">Zackery.</span>
                         </a>
                     </Link>
                     <button
